f52e15ef03864b397f3a48fcfb9c62eed595e90d
S Chopra <sanjaybenu@gmail.com>
2023-06-13 21:29:16 +1000

    added preventdefault in modify and delete files to prevent refresh of page which was preventing it going to the dashboard

--- public/js/modify.js
+++ public/js/modify.js
@@ -1,26 +1,27 @@
-const modify = async () => {
-    const modifyForm = document.getElementById("modifyForm");
+const modify = async (event) => {
+  event.preventDefault();
+  const modifyForm = document.getElementById("modifyForm");
   const blog_Id = modifyForm.getAttribute("data-blog-id");
-  const url = "/api/blog/"+blog_Id
-    const title = document.getElementById('title').value;
-    const description = document.getElementById('description').value;
-  
-    const payload = {
-      title: title,
-      description: description
-    };
-  
-    const response = await fetch(url, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(payload),
-    });
-  
-    if (response.ok) {
-      document.location.replace('/api/users/dashboard');
-    } else {
-      console.log("Failed to modify the blog");
-    }
+  const url = "/api/blog/" + blog_Id;
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+
+  const payload = {
+    title: title,
+    description: description,
   };
-  
-  document.querySelector('#modifyFormBtn').addEventListener('click', modify)
+
+  const response = await fetch(url, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+
+  if (response.ok) {
+    document.location.replace("/api/users/dashboard");
+  } else {
+    console.log("Failed to modify the blog");
+  }
+};
+
+document.querySelector("#modifyFormBtn").addEventListener("click", modify);
